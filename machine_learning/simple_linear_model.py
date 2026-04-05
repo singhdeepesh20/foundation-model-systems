b1081a40c406f2c21e5b77e0fb4d797aee7a8793
Deepesh singh <mr.deepesh.singh.20@gmail.com>
2026-04-05 22:30:19 +0530

    Feature / Target Separation
    Added reshaping of feature into 2D array for scikit-learn compatibility.

--- machine_learning/simple_linear_model.py
+++ machine_learning/simple_linear_model.py
@@ -6,3 +6,8 @@ ITS FITS the best fit line
 Instantiate Linear Regression model
 # Learns relationship: y = (coef * X) + intercept
 model = LinearRegression()
+
+
+# Reshape feature into 2D array as required by scikit-learn
+X = data[['feature']]   # Independent variable (predictor)
+y = data['target']      # Dependent variable (response)
