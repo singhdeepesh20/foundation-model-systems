cc02088c0472ca1beb9f449aaaa6ca021d86758c
Deepesh singh <mr.deepesh.singh.20@gmail.com>
2026-04-07 00:26:04 +0530

    Refactor simple linear regression code structure

--- machine_learning/simple_linear_model.py
+++ machine_learning/simple_linear_model.py
@@ -41,3 +41,11 @@ plt.title("Simple Linear Regression")
 
 plt.legend()
 plt.show()
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import LinearRegression
+
